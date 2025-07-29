a95852707f3f56daa58312ab800de16e62e9cf7a
mortada_korti <korti_mortada@yahoo.com>
2025-07-29 21:53:17 +0900

    Updated config file

--- config.py
+++ config.py
@@ -10,10 +10,9 @@ if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
 # Paths to subfolders
-EXEC_DIR = os.path.join(ROOT_DIR, "exec_files")
-DATA_DIR = os.path.join(ROOT_DIR, "Dataframes")
-MODEL_DIR = os.path.join(ROOT_DIR, "saved_models")
-NOTEBOOK_DIR = os.path.join(ROOT_DIR, "notebook_files")
+EXEC_DIR = os.path.join(ROOT_DIR, "scripts")
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+NOTEBOOKS_DIR = os.path.join(ROOT_DIR, "notebooks")
 RESULTS_DIR = os.path.join(ROOT_DIR, "results")
 
 # Add exec_files for module import
